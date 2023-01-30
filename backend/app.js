4ecef871f09e6c1f6619f60cf1b427720f9a2afc
salma581 <84453156+salma581@users.noreply.github.com>
2023-01-30 18:10:59 +0100

    alot

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+// const path = require('path');
 
-const Thing = require('./models/Thing');
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGO_DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => console.log('Connexion à MongoDB réussie !'))
+
+mongoose.connect('mongodb+srv://salma-p6:melia0000@cluster0.fvuevnk.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
@@ -24,49 +26,11 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body)
-  delete req.body._id;
-  console.log(req.body)
-  const thing = new Thing({
-    ...req.body
-  });
-  console.log(thing)
-  thing.save()
-    .then(() => {
-      console.log("Ok");
-      res.status(201).json({ message: 'Objet enregistré !' });
-    })
-    .catch(error => {
-      console.log("Non Ok");
-      console.log(error);
-      res.status(400).json({ error })
-    });
-});
+app.use('/api/stuff',stuffRoutes);
 
+app.use('/api/auth', userRoutes);
 
-app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-
-});
+// app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
 
