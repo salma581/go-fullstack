dde3fa2e9cbb8e6d88698e5b9b110af9fc5ae5de
salma581 <84453156+salma581@users.noreply.github.com>
2023-02-09 09:13:19 +0100

    token

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,7 +1,8 @@
 const Thing = require('../models/thing');
 const fs = require('fs');
 
-exexports.createThing = (req, res, next) => {
+
+exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
   delete thingObject._id;
   delete thingObject._userId;
@@ -15,6 +16,7 @@ exexports.createThing = (req, res, next) => {
   .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
   .catch(error => { res.status(400).json( { error })})
 };
+
 
 exports.getOneThing = (req, res, next) => {
   Thing.findOne({
@@ -33,28 +35,27 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
+  const thingObject = req.file ? {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete thingObject._userId;
+  Thing.findOne({_id: req.params.id})
+      .then((thing) => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Objet modifié!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
       });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
 };
+
 
 exports.deleteThing = (req, res, next) => {
   Thing.deleteOne({_id: req.params.id}).then(
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,7 +20,7 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
       .then(user => {
-          if (!user) {
+          if (!user) { //=== null
               return res.status(401).json({ error: 'Utilisateur non trouvé !' });
           }
           bcrypt.compare(req.body.password, user.password)
@@ -30,12 +30,13 @@ exports.login = (req, res, next) => {
                   }
                   res.status(200).json({
                       userId: user._id,
+                     
                       token: jwt.sign(
                           { userId: user._id },
                           'RANDOM_TOKEN_SECRET',
                           { expiresIn: '24h' }
                       )
-                  });
+                   });
               })
               .catch(error => res.status(500).json({ error }));
       })
